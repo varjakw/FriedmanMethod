0b3b6a74e4a62ee0ab89b11494558194ee6d5386
varjakw <varjakwolfe2@protonmail.com>
2022-01-28 18:29:53 +0000

    Added decryption

--- cipher.py
+++ cipher.py
@@ -7,19 +7,43 @@ ROOT = tk.Tk()
 ROOT.withdraw()
 
 
-def generateKey(plaintext, keyword):
-    keyword = list(keyword)
-    if len(plaintext) == len(keyword):
-        return (keyword)
+def generateKey(plaintext, key):
+    key = list(key)
+    if len(plaintext) == len(key):
+        return(key)
     else:
-        for i in range(len(plaintext) - len(keyword)):
-            keyword.append(keyword[i % len(keyword)])
-            return "".join(keyword)
+        for i in range(len(plaintext) -
+                       len(key)):
+            key.append(key[i % len(key)])
+    return("" . join(key))
+
+
+def encrypt(plaintext, key):
+    cipher_text = []
+    for i in range(len(plaintext)):
+        x = (ord(plaintext[i]) + ord(key[i])) % 26 # Ei = (Pi + Ki) mod 26
+        x += ord('A')
+        cipher_text.append(chr(x))
+    return("" . join(cipher_text))
+
+def decrypt(cipher_text, key):
+    orig_text = []
+    for i in range(len(cipher_text)):
+        x = (ord(cipher_text[i]) -
+             ord(key[i]) + 26) % 26
+        x += ord('A')
+        orig_text.append(chr(x))
+    return("" . join(orig_text))
 
 
 # the input dialog
-message = input("What's your Message?")
-key = input("What's your Keyword?")
+message = input("What's your Message?").upper()
+keyword = input("What's your Keyword?").upper()
 print("The Key will now be generated from your keyword")
-generateKey(message, key)
+key = generateKey(message, keyword).upper()
 print("The Key is: " + key)
+ciphertext = encrypt(message, key).upper()
+print("The ciphertext is: " + ciphertext)
+plaintext = decrypt(ciphertext, key).upper()
+print("The plaintext is: " + plaintext)
+
