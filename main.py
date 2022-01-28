7156ec4938bc0886b3be99e0515c4bd6fe817ee4
varjakw <varjakwolfe2@protonmail.com>
2022-01-28 22:29:05 +0000

    Editing GUI

--- main.py
+++ main.py
@@ -51,30 +51,33 @@ class app:
         self.menu_btn = tk.Button(self.frame3, text="Main Menu", font=def_font, command=self.menu)
         self.menu_btn.pack()
         self.message_label = tk.Label(self.frame3, text="Enter the message to encrypt", font=def_font)
-        self.message_label.pack()
         self.keyword_label = tk.Label(self.frame3, text="Enter the keyword to encrypt with", font=def_font)
-        # self.uppertext2.pack()
         self.key_label = tk.Label(self.frame3, text="Generated Key", font=def_font)
-        # self.uppertext2.pack()
         self.ciphertext_label = tk.Label(self.frame3, text="Encrypted Text", font=def_font)
-        # self.uppertext2.pack()
         self.message_entry = tk.Entry(font=(def_font, 25))  # width handled by place()
         self.keyword_entry = tk.Entry(font=(def_font, 25))
-        self.message_entry.place(x=30, y=100, width=250, height=40)
-        self.keyword_entry.place(x=30, y=160, width=250, height=40)
+
+        self.message_label.pack(padx=20, pady=20)
+        self.message_entry.place(x=120, y=95, width=250, height=40)
+        #self.message_entry.pack()
+        self.keyword_label.pack(padx=60, pady=50)
+        self.keyword_entry.place(x=120, y=190, width=250, height=40)
+        #self.keyword_entry.pack(padx=10, pady=10)
         self.encrypt_btn = tk.Button(self.frame3, text="Encrypt", font=def_font, command=self.printstuff)
         self.encrypt_btn.pack()
 
         self.key = tk.Entry(font=(def_font, 25))
         self.ciphertext = tk.Entry(font=(def_font, 25))
-        self.key.place(x=30, y=220, width=250, height=40)
-        self.ciphertext.place(x=30, y=280, width=250, height=40)
+        self.key.place(x=120, y=300, width=250, height=40)
+        self.ciphertext.place(x=120, y=380, width=250, height=40)
 
     def printstuff(self):
         messagetext = self.message_entry.get()
         keywordtext = self.keyword_entry.get()
-        print(messagetext)
-        print(keywordtext)
+        print("message: " + messagetext)
+        print("keyword: " + keywordtext)
+
+
 
 
 root = tk.Tk()
